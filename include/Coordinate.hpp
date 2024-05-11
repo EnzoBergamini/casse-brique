7ab145b5bf5098b78853abe70cbdd0fcba2331aa
EnzoBergamini <115539037+EnzoBergamini@users.noreply.github.com>
2024-05-11 11:45:08 +0200

    add coordinate class

---
+++ include/Coordinate.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <utility>
+
+class Coordinate {
+public:
+    Coordinate();
+    Coordinate(int x, int y);
+
+    int getX() const;
+    int getY() const;
+
+    void setX(int x);
+    void setY(int y);
+
+    void print() const;
+
+private:
+    int m_x;
+    int m_y;
+};
---
+++ src/Coordinate.cpp
@@ -0,0 +1,25 @@
+#include "../include/Coordinate.hpp"
+
+Coordinate::Coordinate() : m_x(0), m_y(0) {}
+
+Coordinate::Coordinate(int x, int y) : m_x(x), m_y(y) {}
+
+int Coordinate::getX() const {
+    return m_x;
+}
+
+int Coordinate::getY() const {
+    return m_y;
+}
+
+void Coordinate::setX(int x) {
+    m_x = x;
+}
+
+void Coordinate::setY(int y) {
+    m_y = y;
+}
+
+void Coordinate::print() const {
+    std::cout << "(" << m_x << ", " << m_y << ")" << std::endl;
+}
