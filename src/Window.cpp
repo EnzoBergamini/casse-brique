f5bca0c05670bde375f3d7e3b4ef59f1184dd47b
KillianCvlr <cavalier.killian@gmail.com>
2024-05-13 00:42:37 +0200

    erreur de core aaaaaaaaaah

--- src/Window.cpp
+++ src/Window.cpp
@@ -12,15 +12,21 @@ Window::~Window()
 
 void Window::mainLoop()
 {   
+    std::cout << "Main loop started" << std::endl;
+
     //Commence toujours en MAIN_TITLE
     // Ici seuelemnt en lancement de jeu
+    m_WindowState = WindowState::GAME;
     gameLoop();
+    std::cout << "Game loop ended" << std::endl;
 }
 
 void Window::gameLoop()
 {
     //Commence toujours en GAME
-    Game game;
+    Game game = Game();
+    game.loadBricks("map");
+    std::cout << "Game loaded" << std::endl;
     SDL_Event e;
     while (game.update() != GameState::RUNNING)
     {
